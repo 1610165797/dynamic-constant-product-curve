2c4a53d03f1c62430f92eaf6a2b927497c8a10d8
Shuangge Wang <77814631+1610165797@users.noreply.github.com>
2021-10-26 15:51:25 -0700

    Recursive LaGrange Multiplier

--- Simulation.py
+++ Simulation.py
@@ -2,8 +2,10 @@ import sys
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy import symbols, solve, diff, nsolve
 
-granularity = 1
+granularity = 0.01
+sys.setrecursionlimit(15000)
 
 
 class Curve:
@@ -14,7 +16,7 @@ class Curve:
         self.mkp = mkp
         self.a = x-(y/mkp)
         self.w = k*mkp/y**2
-        self.next = None
+        self.next = []
 
     def get_y(self, x):
         return self.k/self.w/(x-self.a)
@@ -22,22 +24,24 @@ class Curve:
     def print(self):
         print("w is: ", self.w, "a is: ", self.a)
 
-    def plot(self, xf):
+    def plot(self, xf, color):
         x = np.linspace(self.x, xf, 1000)
         y = self.k/self.w/(x-self.a)
-        plt.plot(x, y, 'g')
+        plt.plot(x, y, color)
         plt.grid()
+
+    def onetime_trade(self, xf):
+        return (self.y-self.mkp*(xf-self.x))
 
 
 def exhaustive(curve, xf, storage, count, params):
-    curve.plot(xf)
-    print("plottte")
+    curve.plot(xf, "g")
     print("The point after", count, "trades, is: ", curve.x, curve.y)
     storage.append(curve.y)
     count += 1
     if params["policy"] == "greedy":
-        intercept = (math.sqrt(curve.mkp*curve.w * (curve.mkp*curve.w*xf**2-2*curve.mkp*curve.a * curve.w *
-                                                    xf+3*curve.k+curve.mkp*curve.a**2*curve.w))+curve.mkp*curve.w * (xf+2*curve.a))/(3*curve.mkp*curve.w)
+        intercept = (math.sqrt(xf**2-2*curve.a*xf+3*curve.k /
+                     curve.mkp/curve.w+curve.a**2)+xf+2*curve.a)/3
     elif params["policy"] == "arithmetic":
         intercept = curve.x+0.1
     elif params["policy"] == "geometric":
@@ -54,47 +58,98 @@ def exhaustive(curve, xf, storage, count, params):
         plt.plot(x, storage, 'g')
         return count, curve.get_y(xf)
     else:
-        curve.next = Curve(intercept, curve.get_y(intercept), k, mkp)
-        return exhaustive(curve.next, xf, storage, count, params)
+        curve.next.append(Curve(intercept, curve.get_y(intercept), k, mkp))
+        return exhaustive(curve.next[0], xf, storage, count, params)
 
 
-def opt(curve, xf, n):
+def optimal(curve, xf, n):
     storage = []
-    if(n == 0):
+    if(n == 1):
+        curve.next = None
         return curve.get_y(xf)
     else:
-        intercept=curve.x+granularity
-        if (intercept>=xf):
-            return onetime_trade(curve,xf)
-        while(intercept<xf):
-            next_trade = Curve(intercept, curve.get_y(intercept), k, mkp)
-            intercept+=granularity
-            storage.append(opt(next_trade, xf, n-1))
-        print(n, ":", storage.index(min(storage)))
+        intercept = curve.x+granularity
+        if (intercept >= xf):
+            return curve.onetime_trade(xf)
+        else:
+            while (intercept < xf):
+                next_trade = Curve(intercept, curve.get_y(intercept), k, mkp)
+                curve.next.append(next_trade)
+                intercept += granularity
+                storage.append(optimal(next_trade, xf, n-1))
+        index = storage.index(min(storage))
+        curve.next = curve.next[index]
         return min(storage)
 
 
-def onetime_trade(curve, xf):
-    optimal = curve.y-curve.mkp*(xf-curve.x)
-    return optimal
+def plot_optimal(curve, xf, n):
+    plt.figure()
+    plt.title('Trade History')
+    temp = curve
+    storage = []
+    while(temp != None):
+        temp.plot(xf, "y")
+        print(temp.x)
+        storage.append(temp.x)
+        temp = temp.next
+    plt.figure()
+    x = list(range(0, n))
+    plt.plot(x, storage, 'y')
+    plt.title('x VS n')
 
+
+def set_LaGrange_vector(n):
+    storage = []
+    for i in range(n):
+        storage.append("n"+str(i+1))
+    return storage
+
+
+def formulate_equation(curve, xf, n, list, storage):
+    u = symbols(list[len(list)-n])
+    storage.append(u)
+    if(n == 1):
+        return curve.get_y(u), storage
+    else:
+        next = Curve(u, curve.get_y(u), curve.k, curve.mkp)
+        return formulate_equation(next, xf, n-1, list, storage)
+
+
+def LaGrange(curve, xf, n):
+    result = formulate_equation(curve, xf, n, set_LaGrange_vector(n), [])
+    exp = result[1][-1]-xf
+    lbd = symbols("lbd")
+    exp = result[0]-lbd*exp
+    equations = []
+    result[1].insert(0, lbd)
+    for i in result[1]:
+        equations.append(diff(exp, i))
+    if n==1:
+        return [0,xf]
+    else:
+        guess=list(LaGrange(curve, xf, n-1))
+        guess.insert(1,curve.x)
+        return nsolve(equations, result[1], guess)
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(15000)
     x0 = 100
     y0 = 100
     xf = 110
     mkp = 5
     k = 10000
+    trade_limit = 7
     params = [
         {"policy": "greedy", "params": None},
         {"policy": "arithmetic", "params": 0.1},
         {"policy": "geometric", "params": 0.0001},
         {"policy": "exponential", "params": 0.0001},
     ]
-    head = Curve(x0, y0, k, mkp)
-    #print(plt.figure(),exhaustive(head, xf, [], 0, params[1]))
-    print("optimality: ", opt(head, xf, 10))
-    onetime_trade(head, xf)
+    head1 = Curve(x0, y0, k, mkp)
+    #print("optimality: ", optimal(head1, xf, trade_limit))
+    #plot_optimal(head1, xf, trade_limit)
+    print(LaGrange(head1, xf, trade_limit))
+    #head2 = Curve(x0, y0, k, mkp)
+    #print(plt.figure(),exhaustive(head2, xf, [], 0, params[0]))
+    # head2.onetime_trade(xf)
     plt.grid()
     plt.show()
